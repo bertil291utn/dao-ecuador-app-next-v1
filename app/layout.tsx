7d9907e7ffe15dfb0c8a144a5f0e3944521705d8
Bertil Tandayamo <bertilt4@gmail.com>
2023-05-27 19:12:35 -0500

    add members and proposals pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@components/navbar.component'
+import { navbarItems } from '@placeholders/navbar.placeholders'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className}`}>
+        <Navbar
+          navbarItems={navbarItems}
+        />
+
+        {children}
+
+      </body>
     </html>
   )
 }
---
+++ app/proposals/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const About = () => {
+  return (
+    <div>
+      <p>this is proposals</p>
+    </div>
+  );
+}
+
+export default About;
